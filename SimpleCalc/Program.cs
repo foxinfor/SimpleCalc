d76cb838651ce11924c7b36c36b879e0acef8947
Dmitry Bobr <dimsveta4@gmail.com>
2025-10-06 08:46:57 +0300

    moved input/output into a separate class

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -6,58 +6,13 @@
         {
             FullTask();
         }
-
-        public static double InputNumber(string msg)
-        {
-            Console.Write(msg);
-            while (true)
-            {
-                var num = Console.ReadLine();
-
-                if (double.TryParse(num, out double result))
-                {
-                    return result;
-                }
-                Console.WriteLine("Ошибка: введите корректное число.");
-                Console.Write(msg);
-            }
-        }
-
-        public static string ChoiceOfMathematicalOperation()
-        {
-            while (true)
-            {
-                Console.WriteLine("Выберите операцию: ");
-                Console.WriteLine("1. +");
-                Console.WriteLine("2. -");
-                Console.WriteLine("3. /");
-                Console.WriteLine("4. *");
-                var procedure = Console.ReadLine();
-                switch (procedure)
-                {
-                    case "1":
-                        return "+";
-                    case "2":
-                        return "-";
-                    case "3":
-                        return "/";
-                    case "4":
-                        return "*";
-
-                    default:
-                        Console.WriteLine("Некорректный выбор. Попробуйте снова.\n");
-                        break;
-                }
-            }
-        }
-
         public static void FullTask()
         {
             while (true)
             {
-                double firstNumber = InputNumber("Введите первое число для операции: ");
-                string operation = ChoiceOfMathematicalOperation();
-                double secondNumber = InputNumber("Введите второе число для операции: ");
+                double firstNumber = InputOutput.InputNumber("Введите первое число для операции: ");
+                string operation = InputOutput.ChoiceOfMathematicalOperation();
+                double secondNumber = InputOutput.InputNumber("Введите второе число для операции: ");
                 double result; switch (operation)
                 {
                     case "+":
